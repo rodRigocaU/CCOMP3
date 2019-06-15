9278fcf273644c297dd0ec25a946241739f08d3c
rodRigocaU <42581063+rodRigocaU@users.noreply.github.com>
2019-06-15 11:01:27 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,223 +1,182 @@
-#include <iostream>
-#include <stdlib.h>
-#include "string.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include "string"
+#include "strstream"
+#include "sstream"
+#include "fstream"
+#include "iostream"
 using namespace std;
 
-//template<class U>
-typedef bool(*pb)(int *, int *, bool);
+const int heigth = 20, width = 20;
 
+typedef struct {
+     unsigned char red,green,blue;
+} PPMPixel;
 
-void swaper(int *a, int *b)
+typedef struct {
+     int x, y;
+     PPMPixel *data;
+} PPMImage;
+
+#define CREATOR "RPFELGUEIRAS"
+#define RGB_COMPONENT_COLOR 255
+
+static PPMImage *readPPM(const char *filename)
 {
-    int aux = *a;
-    *a = *b;
-    *b = aux;
-}
+         char buff[16];
+         PPMImage *img;
+         FILE *fp;
+         int c, rgb_comp_color;
+         //open PPM file for reading
+         fp = fopen(filename, "rb");
+         if (!fp) {
+              fprintf(stderr, "Unable to open file '%s'\n", filename);
+              exit(1);
+         }
 
-bool compro(int a, int b)
-{
-    return a > b;
-}
+         //read image format
+         if (!fgets(buff, sizeof(buff), fp)) {
+              perror(filename);
+              exit(1);
+         }
 
-
-bool compare1(int *a, int *b)
-{
-    return *a > *b;
-}
-bool compare2(int *a, int *b)
-{
-
-    return *b > *a;
-
-}
-
-template <class T>
-class functore
-{
-    public:
-    /*T *A;
-    T *B;
-    functore(T *a, T *b)
-    {
-        A = a;
-        B = b;
-    }*/
-    bool operator()(/*string name*/bool (*ns)(T* , T* ), T *A, T *B)
-    {
-        /*if(name == "ascendente")
-        {
-            return *A > *B;
-        }
-        else if(name == "descendente")
-        {
-            return *A < *B;
-        }*/
-        return (*ns)(A,B);
-        
-    }
-    bool compare3(T *, T *);
-    bool compare4(T *, T*);
-};
-
-template <class T>
-bool functore<T>::compare3(T *a, T *b)
-{
-    return *a > *b;
-}
-template <class T>
-bool functore<T>::compare4(T *a , T *b)
-{
-    return *b > *a;
-}
-
-
-template<class T>
-void cocktail(T *p, T siz , bool (*np)(int * , int *))
-{
-    int *startr;
-    int *startl;
-    int *en;
-    int *enl;
-    int aux = 0;
-    bool sorted = true;
-    int vuel;
-    functore<T> c;
-    ///G<T> c; 
-    /*if(false)
-    {
-        startr = p;
-        en =  p + (siz - 1);
-        enl = p;
-        vuel = 1;
-         cout << *startr << '\n';
-         cout << *en << '\n';
-        cout << *enl << '\n';
-        cout << " ENTRE " << '\n';
-        cout << *p << '\n';
-        while(sorted)
-        {
-            cout << "ok "<< '\n';
-            sorted = false;
-            if(vuel == 1)
-            {
-                if(*(startr) > *(startr + 1))
-                {
-                    sorted = true;
-                    swaper((startr),(startr + 1));
-                    startr++;
-                }
-                if(startr == en)
-                {
-                    vuel = 2;
-                    en--;
-                    startl = en;
-                }
-            }
-            else
-            {
-                if(*(startl) < *(startl - 1))
-                {
-                    sorted = true;
-                    swaper((startl), (startl - 1));
-                    startl--;
-                }
-                if(startl == enl)
-                {
-                    vuel = 1;
-                    enl++;
-                    startr = enl;
-                }
-            }
-        }
-    }*/
-    
-    
-    if(true)
-    {
-        startr = p;
-        cout << *startr << '\n';
-        en =  p + (siz - 1);
-        cout << *en << '\n';
-        enl = p;
-        cout << *enl << '\n';
-        vuel = 1;
-        cout << " ENTRE " << '\n';
-        cout << *p << '\n';
-        while(sorted)
-        {
-            cout << "ok "<< '\n';
-            sorted = false;
-            if(vuel == 1)
-            {
-
-                if(/**(startr) < *(startr + 1) (*np)((startr),(startr + 1))*/ c(np,(startr),(startr + 1)) )
-                {
-                    sorted = true;
-                    swaper((startr),(startr + 1));
-                    startr++;
-                }
-                else if( !c(np,(startr),(startr + 1))/*!(*np)((startr),(startr + 1))  (startr) > *(startr + 1) */&& startr != en)
-                {
-                    sorted = true;
-                    startr++;
-                }
-                if(startr == en)
-                {
-                    vuel = 2;
-                    en--;
-                    startl = en;
-                }
-            }
-            else
-            {
-                if(c(np,(startl - 1),(startl)) /*(*np)((startl-1),(startl)) (startl) > *(startl - 1)*/)
-                {
-                    sorted = true;
-                    swaper((startl), (startl - 1));
-                    startl--;
-                }
-                else if(! c(np,(startl-1),(startl))/*(*np)((startl-1),(startl))/**(startl) < *(startl - 1)*/ && startl != enl)
-                {
-                    sorted = true;
-                    startl--;
-                }
-                if(startl == enl)
-                {
-                    vuel = 1;
-                    enl++;
-                    startr = enl;
-                }
-            }
-        }
+    //check the image format
+    if (buff[0] != 'P' || buff[1] != '6') {
+         fprintf(stderr, "Invalid image format (must be 'P6')\n");
+         exit(1);
     }
 
+    //alloc memory form image
+    img = (PPMImage *)malloc(sizeof(PPMImage));
+    if (!img) {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+    //check for comments
+    c = getc(fp);
+    while (c == '#') {
+    while (getc(fp) != '\n') ;
+         c = getc(fp);
+    }
+
+    ungetc(c, fp);
+    //read image size information
+    if (fscanf(fp, "%d %d", &img->x, &img->y) != 2) {
+         fprintf(stderr, "Invalid image size (error loading '%s')\n", filename);
+         exit(1);
+    }
+
+    //read rgb component
+    if (fscanf(fp, "%d", &rgb_comp_color) != 1) {
+         fprintf(stderr, "Invalid rgb component (error loading '%s')\n", filename);
+         exit(1);
+    }
+
+    //check rgb component depth
+    if (rgb_comp_color!= RGB_COMPONENT_COLOR) {
+         fprintf(stderr, "'%s' does not have 8-bits components\n", filename);
+         exit(1);
+    }
+
+    while (fgetc(fp) != '\n') ;
+    //memory allocation for pixel data
+    img->data = (PPMPixel*)malloc(img->x * img->y * sizeof(PPMPixel));
+
+    if (!img) {
+         fprintf(stderr, "Unable to allocate memory\n");
+         exit(1);
+    }
+
+    //read pixel data from file
+    if (fread(img->data, 3 * img->x, img->y, fp) != img->y) {
+         fprintf(stderr, "Error loading image '%s'\n", filename);
+         exit(1);
+    }
+
+    fclose(fp);
+    return img;
 }
-void printArray(int a[], int n)
+void writePPM(const char *filename, PPMImage *img)
 {
-    for (int i = 0; i < n; i++)
-        cout << a[i] << '\t';
+    FILE *fp;
+    //open file for output
+    fp = fopen(filename, "wb");
+    if (!fp) {
+         fprintf(stderr, "Unable to open file '%s'\n", filename);
+         exit(1);
+    }
+
+    //write the header file
+    //image format
+    fprintf(fp, "P6\n");
+
+    //comments
+    fprintf(fp, "# Created by %s\n",CREATOR);
+
+    //image size
+    fprintf(fp, "%d %d\n",img->x,img->y);
+
+    // rgb component depth
+    fprintf(fp, "%d\n",RGB_COMPONENT_COLOR);
+
+    // pixel data
+    fwrite(img->data, 3 * img->x, img->y, fp);
+    fclose(fp);
 }
 
-
-
-int main()
+void changeColorPPM(PPMImage *img)
 {
-    system("COLOR 0A");
-    int a[] = {9,2,3,1,6,5,7,8,4};
-    int siz = sizeof(a)/sizeof(a[0]);
-    bool (*ptsr)(int *, int *) = compare1;
-    ///functore<int> abs;
-    cocktail<int>(a,siz,ptsr);
-    printArray(a,siz);
-    int *z; int *b;
-    int a1 = 6;
-    int b1 = 5;
-    z = &a1;
-    b = &b1;
-    functore<int> hols;///Puedo con parametros o no para la construccion
-    cout << hols.compare3(z,b) << '\n';
-    cout << hols.compare4(z,b) << '\n';
-    cout << hols(/*"descendente"*/ptsr,z,b) << '\n';
-    return 0;
+    int i;
+    string a;
 
+    if(img){
+
+         for(i=0;i<img->x*img->y;i++){
+
+
+              cout << img->data[i].red % 255 << " g " << img->data[i].green%255 << " b " << img->data[i].blue%255 << '\n';
+              std::stringstream s;
+              s << img->data[i].red % 255;
+              a += s.str();
+              std::stringstream b;
+              b << img->data[i].green % 255;
+              a += b.str();
+              std::stringstream c;
+              c << img->data[i].blue % 255;
+              a += c.str();
+              a += " ";
+              /*img->data[i].red=RGB_COMPONENT_COLOR-img->data[i].red;
+              img->data[i].green=RGB_COMPONENT_COLOR-img->data[i].green;
+              img->data[i].blue=RGB_COMPONENT_COLOR-img->data[i].blue;*/
+         }
+    }
+    cout << " Tenemos los colores leidos como " << a << '\n';
+}
+
+int main(){
+    /*PPMImage *image;
+    image = readPPM("4.ppm");
+    changeColorPPM(image);
+    writePPM("4.ppm",image);
+    printf("Press any key...");
+    getchar();*/
+
+    ofstream img("nel.ppm");
+    img << "P6" << '\n';
+    img << width << " " << heigth << '\n';
+    img << "255" << '\n';
+
+    for(int y = 0; y < heigth ; y++)
+    {
+        for(int x = 0; x < width; x++)
+        {
+            int r = x % 255;
+            int g = y % 255;
+            int b = (x * y) % 255;
+            ///cout << r << " "  << g << " " << b << '\n';
+            ///img << r << " " << g << " " << b << '\n';
+        }
+    }
+    system("4.ppm");
 }
